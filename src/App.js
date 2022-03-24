9461b664be0d1846d0c7d3c106b1c42a4f8052ab
AdityaaYadav <yadavadityaa007@gmail.com>
2022-03-24 18:55:45 +0530

    my project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./Componants/Navbar.js";
+import Header  from "./Componants/Header.js";
+
+function App() {
+  return (
+    <>
+  
+    
+   <Navbar/>
+   <Header/>
+</>
+  );
+}
+
+export default App;
---
+++ src/Componants/Header.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+
+
+const Header = () => {
+  return (
+  <div className=" header-wraper">
+      <div className="main-info">
+          <h1>web development and website promotion</h1>
+         
+         <a href="#" className="btn-main-offer">contact me</a>
+      </div>
+  </div>
+  )
+}
+
+export default Header
